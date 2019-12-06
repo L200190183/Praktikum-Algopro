69d2cd446615e22d60b72c2890f6578185b3f87a
Indriarto Dwi Nugroho <55937691+L200190183@users.noreply.github.com>
2019-12-06 18:47:32 +0700

    Add files via upload

---
+++ Praktikum 10/Kegiatan 3 server.py
@@ -0,0 +1,32 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(("", 50009))
+s.listen(5)
+print "Server Siap"
+perintah = ""
+r = 0
+phi = 3.14
+
+while perintah != "keluar":
+    komm, addr = s.accept()
+    while perintah != "keluar":
+        item = komm.recv(1024).lower().split("=")
+        perintah = item [0]
+        if perintah == "keluar":
+            komm.send("done")
+            s.close()
+            break
+        print "pesan: ", perintah
+        if len(item)==2:
+            if perintah == "jari - jari":
+                r=int(item[1])
+                komm.send("jari - jari disimpan")
+            else:
+                komm.send("Pesan tidak diketahui")
+        elif perintah == "hitung":
+            L=float(4*phi*r**2)
+            response = "Luas bola dengan jari - jari {} adalah {}".format(r,L)
+            komm.send(response)
+        else:
+            komm.send("Pesan tidak diketahui")
